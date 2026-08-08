36d925f61260afb6a44bf0c52c8a1ffec4a90806
Ranjit-Dit <ranjit.dit@gmail.com>
2026-08-08 13:41:02 +0530

    day-1 install xcode for clang

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "My first c++ Program." << endl;
+    cout << "The sum of 2 and 4  = " << 5 << endl;
+    cout << " 7 + 8 = " << 7 + 8 << endl;
+
+    return 0; 
+}
